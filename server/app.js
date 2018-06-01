c374ccde154906dc7508313ea6d198e81aa6062f
Richard Zhunio <Richard-284@hotmail.com>
2018-06-01 11:40:06 -0400

    Added /uploads to serve static files.

--- server/app.js
+++ server/app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../uploads')));
 
 // User defined middleware Routes
 // aaaaaaaa asadasnfsifnof
