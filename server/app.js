b0e287ff0fa13ec244b99854b87bf3a9e3729d90
Richard Zhunio <Richard-284@hotmail.com>
2018-06-01 12:09:12 -0400

    Fixed /uploads

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ app.use( express.static( path.join( __dirname, '../public' ) ) );
 app.use( '/', index );
 app.use( '/node-builder', nodeBuilder );
 app.use( '/ft_test', updateFusionTables );
-app.use( '/uploads', express.static( path.join( __dirname, '../uploads' ) );
+app.use( '/uploads', express.static( path.join( __dirname, '../uploads' ) ) );
 
 // Error Logging, catching, and handling
 app.use( logger.error );
